2f8812a95af978b0f562fecddcbabb58a154cf08
bettinavass <vassbetka@gmail.com>
2018-09-04 19:27:13 +0000

    Finished the algorithm

--- sequence.py
+++ sequence.py
@@ -1,1 +1,17 @@
-#comment
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+num1 = 1
+num2 = 2
+num3 = 3
+
+print(num1)
+print(num2)
+print(num3)
+
+for i in range(3, n):
+    max_num = num1 + num2 + num3
+    print(max_num)
+    num1 = num2
+    num2 = num3
+    num3 = max_num
+  
